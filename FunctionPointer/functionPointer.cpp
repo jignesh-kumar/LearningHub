a76cb2eb1eaefd0574243704d4b1370b42ae9bca
Jigneshkumar Vadaviya <jignesh.vadaviya@yahoo.com>
2022-11-19 18:04:50 +0530

    Some more example for function pointer
    also added readme file for function pointer concept

--- FunctionPointer/functionPointer.cpp
+++ FunctionPointer/functionPointer.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -21,6 +22,49 @@ void func2(void (*funcptr)())
     funcptr();
 }
 
+// Here is a comparison function that sorts in ascending order
+bool ascending(int x, int y)
+{
+    return x > y; // swap if the first element is greater than the second
+}
+
+// Here is a comparison function that sorts in descending order
+bool descending(int x, int y)
+{
+    return x < y; // swap if the second element is greater than the first
+}
+
+// This function prints out the values in the array
+void printArray(int *array, int size)
+{
+    for (int index = 0; index < size; ++index)
+    {
+        cout << array[index] << " " ;
+    }
+
+    cout << endl;
+}
+
+// Note our user-defined comparison is the third parameter
+void selectionSort(int *array, int size, bool (*comparisonFcn)(int, int))
+{
+    for (int startIndex = 0; startIndex < size; ++startIndex)
+    {
+        // bestIndex is the index of the smallest/largest element we've encountered so far.
+        int bestIndex = startIndex;
+        // Look for smallest/largest element remaining in the array (starting at startIndex+1)
+        for (int currentIndex = startIndex + 1; currentIndex < size; ++currentIndex)
+        {
+            // If the current element is smaller/larger than our previously found smallest
+            if (comparisonFcn(array[bestIndex], array[currentIndex])) // COMPARISON DONE HERE
+                // This is the new smallest/largest number for this iteration
+                bestIndex = currentIndex;
+        }
+        // Swap our start element with our smallest/largest element
+        swap(array[startIndex], array[bestIndex]);
+    }
+}
+
 int main()
 {
     cout << "********************************** Example 1 ***************************************" << endl;
@@ -34,7 +78,12 @@ int main()
         char myData[20];
         void (*funcPtr)(char *data);
         funcPtr = printName;
+
+        // Calling a function using a function pointer via implicit dereference
         funcPtr("My Name is Jonty");
+
+        // Calling a function using a function pointer via explicit dereference
+        (*funcPtr)("This is the world!");
     }
     cout << "********************************** Example 3 ***************************************" << endl;
     {
@@ -55,6 +104,25 @@ int main()
             We want funcPtr to be assigned the address of function myFunction, not the return value from function myFunction().
             So no parenthesis are needed.
         */
+    }
+
+    cout << "********************************** Example 5 ***************************************" << endl;
+    {
+        // Passing functions as arguments to other functions
+        int array[9] = {3, 7, 9, 5, 6, 1, 8, 2, 4};
+
+        cout << "Array Value is : ";
+        printArray(array, 9);
+        
+        // Sort the array in descending order using the descending() function
+        selectionSort(array, 9, descending);
+        cout << "Array Value after sort in descending : ";
+        printArray(array, 9);
+
+        // Sort the array in ascending order using the ascending() function
+        selectionSort(array, 9, ascending);
+        cout << "Array Value after sort in ascending  : ";
+        printArray(array, 9);
     }
 
     cout << "********************************** End *********************************************" << endl;
