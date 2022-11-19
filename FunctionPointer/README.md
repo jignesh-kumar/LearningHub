e413483cacea953a5f5a5df6cdcefc6d617fbc08
Jigneshkumar Vadaviya <jignesh.vadaviya@yahoo.com>
2022-11-19 16:56:48 +0530

    Added more example on function pointer

---
+++ FunctionPointer/README.md
@@ -0,0 +1,6 @@
+Function Pointer
+
+You know that a pointer is a variable that holds the address of another variable. Function pointers are similar, except that instead of pointing to variables, they point to functions!
+
+Just like it is possible to declare a non-constant pointer to a normal variable, it’s also possible to declare a non-constant pointer to a function. In the rest of this lesson, we’ll examine these function pointers and their uses. Function pointers are a fairly advanced topic, and the rest of this lesson can be safely skipped or skimmed by those only looking for C++ basics.
+
--- FunctionPointer/functionPointer.cpp
+++ FunctionPointer/functionPointer.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include<stdio.h>
 
 using namespace std;
-
 
 void myFunction()
 {
@@ -14,32 +12,50 @@ void printName(char *data)
     cout << "The data is " << data << endl;
 }
 
-void func1()  
-{  
-    cout << "func1 is called" << endl;  
-}  
-void func2(void (*funcptr)())  
-{  
-    funcptr();  
-}  
+void func1()
+{
+    cout << "func1 is called" << endl;
+}
+void func2(void (*funcptr)())
+{
+    funcptr();
+}
 
 int main()
 {
-    cout << "********************************** Example 3 ***************************************" << endl;
-
-    printf("address of function main() is :%p\n", main);
-    printf("address of function myFunction() is : %p\n", myFunction);
+    cout << "********************************** Example 1 ***************************************" << endl;
+    {
+        cout << "address of function main is       : " << reinterpret_cast<void *>(main) << endl;
+        cout << "address of function myFunction is : " << reinterpret_cast<void *>(myFunction) << endl;
+    }
 
     cout << "********************************** Example 2 ***************************************" << endl;
-
-    char myData[20];
-    void(*funcPtr)(char *data);
-    funcPtr = printName;
-    funcPtr("My Name is Jonty");
-
+    {
+        char myData[20];
+        void (*funcPtr)(char *data);
+        funcPtr = printName;
+        funcPtr("My Name is Jonty");
+    }
     cout << "********************************** Example 3 ***************************************" << endl;
+    {
+        func2(func1);
+    }
 
-    func2(func1);
+    cout << "********************************** Example 4 ***************************************" << endl;
+    {
+        // Assigning a function to a function pointer
+        void (*funcPtr)(void) = func1; // funcPtr points to function func1
+        funcPtr();
+
+        funcPtr = myFunction; // funcPtr now points to function myFunction
+        funcPtr();
+
+        // funcPtr = myFunction();
+        /*  This would actually assign the return value from a call to function myFunction() to funcPtr, which isn’t what we want.
+            We want funcPtr to be assigned the address of function myFunction, not the return value from function myFunction().
+            So no parenthesis are needed.
+        */
+    }
 
     cout << "********************************** End *********************************************" << endl;
     return 0;
